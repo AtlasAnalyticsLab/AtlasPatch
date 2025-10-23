07302fd4191bffbcb7d01daffe73b65a432d3068
Yousef Kotp <yousefkotp@outlook.com>
2025-10-23 14:23:55 -0400

    remove unused inference dataset

--- slide_processor/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Datasets for segmentation tasks."""
-
-from .inference_dataset import InferenceSegmentationDataset, get_bounding_box
-
-__all__ = ["InferenceSegmentationDataset", "get_bounding_box"]
